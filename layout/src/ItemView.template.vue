e9b89e0e24002fdc180d129782193857f33b405c
Trevor <xizhouh@gmail.com>
2018-05-15 15:22:17 -0500

    feat: remove gitalk & add issue comments

--- layout/src/ItemView.template.vue
+++ layout/src/ItemView.template.vue
@@ -20,17 +20,52 @@
           {{ item.created_at | timeAgo }}
         </p>
       </div>
-      <div v-if="item.body" class="content" v-html="htmlResource" v-highlight></div>
+      <div v-if="item.body" class="content" v-html="articleResource" v-highlight></div>
       <div class="item-view-footer">
         <p class="meta">
           Updated {{ item.updated_at | timeAgo }}
         </p>
       </div>
-      <div v-if="this.$_config.gitalk.useGitalk" id="item-view-comments"></div>
+      <div class="item-view-comments">
+        <div class="comments-content content" v-show="open && item.comments">
+          <li v-for="comment in comments" :key="comment.id">
+            <a class="avatar" :href="comment.user.html_url"
+              target="_blank"
+              rel="noopener">
+              <img :src="comment.user.avatar_url" alt="comments-avatar">
+            </a>
+            <div class="body">
+              <a :href="comment.user.html_url" target="_blank" rel="noopener">
+                {{ comment.user.login }}
+              </a>
+              <span>Created {{ comment.created_at | timeAgo }}</span>
+              <div class="markdown-body" v-html="comment.body" v-highlight>
+              </div>
+            </div>
+          </li>
+        </div>
+        <a v-show="!open && item.comments" @click="open = !open" class="button">View comments({{ item.comments }})</a>
+        <a v-show="!item.comments"
+          class="button"
+          :href="`https://github.com/${this.$_config.user}/${this.$_config.repo}/issues/${item.number}#new_comment_field`"
+          target="_blank"
+          rel="noopener">Add new comment</a>
+        <a v-show="open && item.comments"
+          class="button"
+          :href="`https://github.com/${this.$_config.user}/${this.$_config.repo}/issues/${item.number}#new_comment_field`"
+          >Add comments</a>
+      </div>
     </template>
   </div>
 </template>
 
 <script>
 import 'highlight.js/styles/agate.css';
+export default {
+  data () {
+    return {
+      open: false
+    }
+  },
+}
 </script>
